35bc7cba6fe2e0bbc293ca4385d98c989988a736
chenhz <843207296@qq.com>
2018-09-06 00:16:06 +0800

    hello world

---
+++ tpos/src/main/java/com/chenhz/tpos/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.chenhz.tpos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author CHZ
+ * @create 2018/9/5
+ */
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping("one")
+    public String test(){
+        return "success";
+    }
+}
---
+++ tpos/src/test/java/com/chenhz/tpos/TposApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chenhz.tpos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TposApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
