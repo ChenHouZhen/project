30f8eff7f8ca74b1397710c6f2ab3062f23190f0
chenhz <843207296@qq.com>
2018-09-10 21:29:30 +0800

    faker

--- faker/src/main/java/com/chenhz/faker/utils/Fakers.java
+++ faker/src/main/java/com/chenhz/faker/utils/Fakers.java
@@ -1,17 +1,86 @@
 package com.chenhz.faker.utils;
 
+import com.github.javafaker.Address;
+import com.github.javafaker.Book;
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Locale;
+
 public class Fakers {
 
-    Logger log = LoggerFactory.getLogger(Fakers.class);
+    static Logger log = LoggerFactory.getLogger(Fakers.class);
 
-    public void run(){
+    static Faker faker ;
 
+    @BeforeClass
+    public static void init(){
+        log.info("加载实例......");
+        faker = new Faker(new Locale("zh_CN"));
     }
 
-    public static void main(String[] args) {
+    /**
+     * 地址信息类
+     */
+    @Test
+    public void address(){
+        log.info("地址API...");
+        Address address = faker.address();
+        log.info("address.city():"+address.city());  // 市
+        log.info("address.latitude():"+address.latitude());
+        log.info("address.longitude():"+address.longitude());
+        log.info("address.streetAddress():"+address.streetAddress()); //地址
+        log.info("address.cityName():"+address.cityName());
+        log.info("address.state():"+address.state());  //省
 
+/*        log.info("address.country():"+address.country());
+        log.info("address.countryCode():"+address.countryCode());
+        log.info("address.firstName():"+address.firstName());
+        log.info("address.lastName():"+address.lastName());
+        log.info("address.secondaryAddress():"+address.secondaryAddress());
+        log.info("address.stateAbbr():"+address.stateAbbr());
+        log.info("address.streetAddressNumber():"+address.streetAddressNumber());
+        log.info("address.streetName():"+address.streetName());
+        log.info("address.timeZone():"+address.timeZone());
+        log.info("address.zipCode():"+address.zipCode());*/
+        log.info("-------------------------------");
     }
+
+    /**
+     * 名字
+     */
+    @Test
+    public void name(){
+        log.info("名字API......");
+        Name name = faker.name();
+        log.info("name.name()："+name.name());
+        log.info("name.fullName()："+name.fullName());
+        log.info("name.lastName()："+name.lastName());
+        log.info("name.firstName()："+name.firstName());
+
+/*        log.info("name.username()："+name.username());
+        log.info("name.nameWithMiddle()："+name.nameWithMiddle());
+        log.info("name.prefix()："+name.prefix());
+        log.info("name.suffix()："+name.suffix());
+        log.info("name.title()："+name.title());*/
+        log.info("-------------------------------");
+    }
+
+
+    @Test
+    public void book(){
+        Book book = faker.book();
+        log.info("书籍API......");
+        log.info("book.author():"+book.author());
+        log.info("book.genre():"+book.genre());
+        log.info("book.publisher():"+book.publisher());
+        log.info("book.title():"+book.title());
+        log.info("-------------------------------");
+    }
+
+
 }
