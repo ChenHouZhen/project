99980b334d311ea4c7eeefe25aa1662c01605823
chenhouzhen <chenhouzhen@hqjy.com>
2018-10-29 09:03:22 +0800

    excel

--- excel/src/main/java/com/chenhz/excel/ExcelApplication.java
+++ excel/src/main/java/com/chenhz/excel/ExcelApplication.java
@@ -1,13 +1,12 @@
-package com.chenhz.neo4j;
+package com.chenhz.excel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-public class Neo4jApplication {
+public class ExcelApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Neo4jApplication.class, args);
+        SpringApplication.run(ExcelApplication.class, args);
     }
 }
---
+++ excel/src/main/java/com/chenhz/excel/utils/Path.java
@@ -0,0 +1,8 @@
+package com.chenhz.excel.utils;
+
+public class Path {
+    public static void main(String[] args) {
+        Path path = new Path();
+        System.out.println(path.getClass().getResource(""));
+    }
+}
