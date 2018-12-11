cc2181fe914256fd7b236c72a15efbae55c9db40
chenhz <843207296@qq.com>
2018-12-11 20:36:06 +0800

    dd

--- excel/src/main/java/com/chenhz/excel/controller/ExcelController.java
+++ excel/src/main/java/com/chenhz/excel/controller/ExcelController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/excel")
 public class ExcelController {
@@ -13,6 +17,8 @@ public class ExcelController {
     @PostMapping("/import")
     public String importExcel(@RequestParam("file")MultipartFile file){
         System.out.println(file.getOriginalFilename());
-        return "";
+        List a = new ArrayList();
+        a.add("1");
+        List<String> ss= a.stream().map(d ->(d+"1")).collect(Collectors.toList())
     }
 }
